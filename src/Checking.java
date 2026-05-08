8cd725fd21a49f32f85cf28c6e1d19b23ab97a86
Varsha <varshaacharya2017@gmail.com>
2026-05-08 23:39:38 +0530

    Changes made in GitDemo

---
+++ src/Checking.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class Checking 
+{
+	@Test
+	public void onelineadd()
+	{
+		System.out.println("I added one line here");
+	}
+	
+	@Test
+	public void twolineadd()
+	{
+		System.out.println("I added two line here");
+	}
+}
